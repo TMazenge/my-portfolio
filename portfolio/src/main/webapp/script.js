36aec91c32c2044b3cfa2c8d5aee9b5f3b8d626c
Timothy Mazenge <timmazenge@gmail.com>
2020-05-29 12:06:57 -0500

    Update script and html

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -53,3 +53,25 @@ function changeImg(){
 
 // Run function when page loads
 window.onload=changeImg;
+
+
+var countDownDate = new Date("May 5, 2022 16:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("grad").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("grad").innerHTML = "EXPIRED";
+  }
+}, 1000);
